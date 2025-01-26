69afb90cf8dbcc01115492aafbb4eb3c12c68ce4
Teoom <ararama199915@gmail.com>
2025-01-26 10:34:43 +0700

    add functionExpression

---
+++ scripts/functionExpression.js
@@ -0,0 +1,27 @@
+const operations = {
+    multiply: "*",
+    divide: "/",
+    addition: "+",
+    subtraction: "-",
+  };
+  
+  const calculate = function (num1, num2, operation) {
+    if (typeof num1 !== "number" || typeof num2 !== "number")
+      return "Передайте число";
+  
+    switch (operation) {
+      case "*":
+        return num1 * num2;
+      case "/":
+        return num1 / num2;
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+    }
+  };
+  
+  const selectedOperation = "multiply";
+  console.log(calculate(6, 3, operations[selectedOperation])); // 18
+  
+  
